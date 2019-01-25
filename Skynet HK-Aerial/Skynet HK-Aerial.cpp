bafb9709141ba77cb7dc4dc9ac34c193d78d9c33
nornguye <normannguyen3@gmail.com>
2019-01-25 13:27:27 -0700

    Progress 1

--- Skynet HK-Aerial/Skynet HK-Aerial.cpp
+++ Skynet HK-Aerial/Skynet HK-Aerial.cpp
@@ -1,14 +1,33 @@
 
 #include <iostream>
+#include <String>
+#include <>
 
 using namespace std;
 
 int main ()
 {
+	int searchGridHighNumber = 64;
+	int searchGridLowNumber = 1;
+	int enemyLocation = rand() % 64 + 1;
 	while (true)
 	{ 
 		int targetLocationPrediction = ((searchGridHighNumber - searchGridLowNumber) / 2) + searchGridLowNumber;
-		cout << "Check!" << endl;
+		if (targetLocationPrediction == enemyLocation)
+		{
+
+		}
+		else if (targetLocationPrediction == enemyLocation)
+		{
+
+		}
+		else if (targetLocationPrediction == enemyLocation)
+		{
+
+		}
+		else
+		cout << "Sir, we've a problem with the drones!" << endl;
+		cout << "We call for maintence!" << endl;
 		system("pause");
 	}
 }
