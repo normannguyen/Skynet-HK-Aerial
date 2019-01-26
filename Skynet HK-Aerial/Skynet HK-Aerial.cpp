b7140cb492dbce899aa67457ee7699f321a3e0ef
nornguye <normannguyen3@gmail.com>
2019-01-25 20:40:29 -0700

    Progress 2

--- Skynet HK-Aerial/Skynet HK-Aerial.cpp
+++ Skynet HK-Aerial/Skynet HK-Aerial.cpp
@@ -1,7 +1,8 @@
 
 #include <iostream>
 #include <String>
-#include <>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -10,24 +11,43 @@ int main ()
 	int searchGridHighNumber = 64;
 	int searchGridLowNumber = 1;
 	int enemyLocation = rand() % 64 + 1;
-	while (true)
+	int targetPredictionPing = 0;
+	bool enemyTargeted = false;
+	cout << "System Processing 8x8 Grid..." << endl;
+	cout << "System: The enemy is somewhere in #" << targetPredictionPing << endl;
+	cout << "Skynet HK-Aerial Initializing Software...." << endl;
+	while (false)
 	{ 
 		int targetLocationPrediction = ((searchGridHighNumber - searchGridLowNumber) / 2) + searchGridLowNumber;
+		targetPredictionPing++;
 		if (targetLocationPrediction == enemyLocation)
 		{
-
+			cout << "Skynet HK-Aerial Radar sending out ping #" << targetPredictionPing << endl;
+			cout << "Sir, we've a problem with the drones!" << endl;
+			cout << "We call for maintence!" << endl;
+			enemyTargeted = true;
 		}
-		else if (targetLocationPrediction == enemyLocation)
+		else if (targetLocationPrediction > enemyLocation)
 		{
-
+			cout << "Skynet HK-Aerial Radar sending out ping #" << targetPredictionPing << endl;
+			searchGridHighNumber = targetLocationPrediction - 1;
+			cout << "The new searchGridHighNumber = " << searchGridHighNumber << endl;
+			cout << "" << endl;
 		}
-		else if (targetLocationPrediction == enemyLocation)
+		else if (targetLocationPrediction < enemyLocation)
 		{
-
+			searchGridLowNumber = targetLocationPrediction + 1;
+			cout << "Sir, we've a problem with the drones!" << endl;
+			cout << "We call for maintence!" << endl;
+			cout << "The new searchGridHighNumber = " << searchGridLowNumber << endl;
 		}
 		else
-		cout << "Sir, we've a problem with the drones!" << endl;
-		cout << "We call for maintence!" << endl;
-		system("pause");
+		{
+			cout << "Sir, we've a problem with the drones!" << endl;
+			cout << "We call for maintence!" << endl;
+			enemyTargeted = true;
+		}
 	}
+	system("Pause");
+	return 0;
 }
